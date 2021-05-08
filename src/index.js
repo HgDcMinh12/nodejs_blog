495f91c5426cc8eb51b8b16365d389af170902dd
HgDcMinh12 <tysonhoang1211@gmail.com>
2021-05-08 12:27:26 +0700

    edit code to MVC concept

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', handlebars({
     extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resource/views'));
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resource/views'))
 
-
-app.get('/', (req, res) =>
-    res.render('home'))
-
-app.get('/news', (req, res) =>
-    res.render('news'))
+// Route init
+route(app)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
