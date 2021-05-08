dda3b651caea27dfb0b35a0ba15021db366412a1
HgDcMinh12 <tysonhoang1211@gmail.com>
2021-05-08 17:32:31 +0700

    add model [MVC]

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const route = require('./routes');
+const db = require('./config/db')
+
+// Connect to DB
+db.connect();
+
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP logger
-                                        app.use(morgan('combined'));
+app.use(morgan('combined'));
 
 //Template engine
 app.engine(
@@ -20,11 +25,11 @@ app.engine(
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resource/views'));
+app.set('views', path.join(__dirname, 'resource','views'));
 
 // Route init
 route(app);
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`),
+    console.log(`App listening at http://localhost:${port}`),
 );
